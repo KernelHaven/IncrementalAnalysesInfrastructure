f207e4a1cdfe956043e490e2f02b2464b9711b43
moritz <moritzf@gmail.com>
2019-04-10 17:36:29 +0200

    Add identification of differences between two SourceFiles

---
+++ src/net/ssehub/kernel_haven/incremental/util/SourceFileChangeDetector.java
@@ -0,0 +1,238 @@
+package net.ssehub.kernel_haven.incremental.util;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+import net.ssehub.kernel_haven.code_model.CodeElement;
+import net.ssehub.kernel_haven.code_model.SourceFile;
+import net.ssehub.kernel_haven.variability_model.VariabilityModel;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class CodeFileComparator.
+ * 
+ * @author Moritz
+ */
+public class SourceFileChangeDetector {
+
+    /**
+     * The Enum Consideration.
+     */
+    public static enum Consideration {
+
+        /** The all elements. */
+        ANY_CHANGE,
+        /** The ignore linechange. */
+        ANY_CHANGE_EXCEPT_LINECHANGE,
+        /** The only variability change. */
+        ONLY_VARIABILITY_CHANGE;
+    }
+
+    /** The var model A. */
+    protected VariabilityModel varModelA;
+
+    /** The var model B. */
+    protected VariabilityModel varModelB;
+
+    /** The consideration. */
+    private Consideration consideration;
+
+    /**
+     * Instantiates a new code file comparator.
+     *
+     * @param consideration the consideration
+     * @param varModelA     the var model A
+     * @param varModelB     the var model B
+     */
+    public SourceFileChangeDetector(Consideration consideration, VariabilityModel varModelA,
+            VariabilityModel varModelB) {
+        this.consideration = consideration;
+        this.varModelA = varModelA;
+        this.varModelB = varModelB;
+    }
+
+    /**
+     * Instantiates a new source file change detector.
+     */
+    protected SourceFileChangeDetector() {
+
+    }
+
+    /**
+     * Checks for changed.
+     *
+     * @param fileA the file A
+     * @param fileB the file B
+     * @return true, if changed
+     */
+    public boolean hasChanged(SourceFile<?> fileA, SourceFile<?> fileB) {
+        boolean changed = false;
+        if (!fileA.equals(fileB)) {
+            if (this.consideration == Consideration.ANY_CHANGE) {
+                changed = true;
+            } else {
+
+                if (this.consideration == Consideration.ONLY_VARIABILITY_CHANGE) {
+                    Set<CodeElement<?>> relevancyA =
+                            collectRelevantElements(fileA, new LinuxFormulaRelevancyChecker(varModelA, true));
+                    Set<CodeElement<?>> relevancyB =
+                            collectRelevantElements(fileB, new LinuxFormulaRelevancyChecker(varModelB, true));
+                    changed = !isStructureSame(fileA, fileB, relevancyA, relevancyB);
+                } else {
+                    changed = !isStructureSame(fileA, fileB, null, null);
+                }
+            }
+
+        }
+        return changed;
+    }
+
+    /**
+     * Collects {@link CodeElement}s that are considered relevant from a given
+     * {@link SourceFile}. If {@link FormulaRelevancyChecker} considers an element
+     * to be relevant, all of its parents as well as children will be considered
+     * relevant as well.
+     *
+     * @param file    the file
+     * @param checker the checker
+     * @return the sets the
+     */
+    protected Set<CodeElement<?>> collectRelevantElements(SourceFile<?> file, LinuxFormulaRelevancyChecker checker) {
+        Set<CodeElement<?>> relevantElements = new HashSet<CodeElement<?>>();
+        for (CodeElement<?> element : file) {
+            collectRelevantElements(element, checker, new HashSet<CodeElement<?>>(), new HashSet<CodeElement<?>>(),
+                    relevantElements);
+        }
+        return relevantElements;
+    }
+
+    /**
+     * Collects {@link CodeElement}s that are considered relevant from a given
+     * {@link CodeElement}. If {@link FormulaRelevancyChecker} considers an element
+     * to be relevant, all of its parents as well as children will be considered
+     * relevant as well.
+     *
+     * @param currentElement           the current element
+     * @param checker                  the checker
+     * @param parents                  the parents
+     * @param directlyRelevantElements the directly relevant elements
+     * @param relevantElements         the relevant elements
+     */
+    protected void collectRelevantElements(CodeElement<?> currentElement, LinuxFormulaRelevancyChecker checker,
+            Set<CodeElement<?>> parents, Set<CodeElement<?>> directlyRelevantElements,
+            Set<CodeElement<?>> relevantElements) {
+        // if the path contains an element that is directly relevant,
+        // currentElement is relevant as well.
+        if (!Collections.disjoint(directlyRelevantElements, parents)) {
+            relevantElements.add(currentElement);
+            // Otherwise check if the element is relevant on its own (=directly relevant)
+        } else if (checker.visit(currentElement.getPresenceCondition())) {
+            relevantElements.add(currentElement);
+            directlyRelevantElements.add(currentElement);
+            // if the element is element on its own, it also makes all of its parents
+            // relevant
+            relevantElements.addAll(parents);
+        }
+
+        // After the current element itself was handled, take care of its nested
+        // elements
+        int nestedCount = currentElement.getNestedElementCount();
+        if (nestedCount > 0) {
+            // Create a copy of the parent list and add currentElement as a parent
+            Set<CodeElement<?>> newParents = new HashSet<CodeElement<?>>(parents);
+            newParents.add(currentElement);
+            for (int i = 0; i < nestedCount; i++) {
+                CodeElement<?> nestedElement = currentElement.getNestedElement(i);
+                collectRelevantElements(nestedElement, checker, newParents, directlyRelevantElements, relevantElements);
+            }
+        }
+
+    }
+
+    /**
+     * Checks for changes within the structure of two given source files.
+     *
+     * @param fileA                   the file A
+     * @param fileB                   the file B
+     * @param relevantElementsInFileA the relevancy A
+     * @param relevantElementsInFileB the relevancy B
+     * @return true, if unchanged
+     */
+    protected boolean isStructureSame(SourceFile<?> fileA, SourceFile<?> fileB,
+            Set<CodeElement<?>> relevantElementsInFileA, Set<CodeElement<?>> relevantElementsInFileB) {
+
+        List<CodeElement<?>> fileAelements =
+                getListOfRelevantNestedElements((Iterable<CodeElement<?>>) fileA, relevantElementsInFileA);
+        List<CodeElement<?>> fileBelements =
+                getListOfRelevantNestedElements((Iterable<CodeElement<?>>) fileB, relevantElementsInFileB);
+
+        boolean same = fileAelements.size() == fileBelements.size();
+
+        if (same) {
+            for (int i = 0; same && i < fileAelements.size(); i++) {
+                CodeElement<?> fileAelement = fileAelements.get(i);
+                CodeElement<?> fileBelement = fileBelements.get(i);
+                same = isStructureSame(fileAelement, fileBelement, relevantElementsInFileA, relevantElementsInFileB);
+            }
+        }
+
+        return same;
+    }
+
+    /**
+     * Checks for changes within the structure of both elements and their respective
+     * nested elements.
+     *
+     * @param fileAElement            the file A element
+     * @param fileBElement            the file B element
+     * @param relevantElementsInFileA the relevancy A
+     * @param relevantElementsInFileB the relevancy B
+     * @return true, if unchanged
+     */
+    protected boolean isStructureSame(CodeElement<?> fileAElement, CodeElement<?> fileBElement,
+            Set<CodeElement<?>> relevantElementsInFileA, Set<CodeElement<?>> relevantElementsInFileB) {
+
+        boolean same = fileAElement.getPresenceCondition().equals(fileBElement.getPresenceCondition());
+        if (same) {
+
+            List<CodeElement<?>> fileAelements =
+                    getListOfRelevantNestedElements((Iterable<CodeElement<?>>) fileAElement, relevantElementsInFileA);
+            List<CodeElement<?>> fileBelements =
+                    getListOfRelevantNestedElements((Iterable<CodeElement<?>>) fileBElement, relevantElementsInFileB);
+            same = fileAelements.size() == fileBelements.size();
+            if (same) {
+                for (int i = 0; same && i < fileAelements.size(); i++) {
+                    CodeElement<?> fileAelement = fileAelements.get(i);
+                    CodeElement<?> fileBelement = fileBelements.get(i);
+                    same = fileAelement.getPresenceCondition().equals(fileBelement.getPresenceCondition())
+                            && isStructureSame(fileAelement, fileBelement, relevantElementsInFileA,
+                                    relevantElementsInFileB);
+                }
+            }
+        }
+
+        return same;
+    }
+
+    /**
+     * Gets the relevant blocks.
+     *
+     * @param parentOfNestedElements the elements
+     * @param relevantElementsInFile the relevancy
+     * @return the relevant blocks
+     */
+    protected List<CodeElement<?>> getListOfRelevantNestedElements(Iterable<CodeElement<?>> parentOfNestedElements,
+            Set<CodeElement<?>> relevantElementsInFile) {
+        List<CodeElement<?>> reduced = new LinkedList<CodeElement<?>>();
+        for (CodeElement<?> element : parentOfNestedElements) {
+            if (relevantElementsInFile == null || relevantElementsInFile.contains(element)) {
+                reduced.add(element);
+            }
+        }
+        return reduced;
+    }
+
+}
---
+++ test/net/ssehub/kernel_haven/incremental/util/SourceFileChangeDetectorTest.java
@@ -0,0 +1,270 @@
+package net.ssehub.kernel_haven.incremental.util;
+
+import static net.ssehub.kernel_haven.util.logic.FormulaBuilder.not;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+import net.ssehub.kernel_haven.code_model.CodeBlock;
+import net.ssehub.kernel_haven.code_model.CodeElement;
+import net.ssehub.kernel_haven.code_model.SourceFile;
+import net.ssehub.kernel_haven.variability_model.VariabilityModel;
+import net.ssehub.kernel_haven.variability_model.VariabilityVariable;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class SourceFileChangeDetectorTest.
+ */
+public class SourceFileChangeDetectorTest extends SourceFileChangeDetector {
+
+    /**
+     * Creates a test variability model with the variables ALPHA, and BETA.
+     * 
+     * @return A test {@link VariabilityModel}.
+     */
+    private static VariabilityModel createTestVariabilityModel() {
+        Set<VariabilityVariable> vars = new HashSet<>();
+        vars.add(new VariabilityVariable("ALPHA", "bool"));
+        vars.add(new VariabilityVariable("BETA", "bool"));
+
+        VariabilityModel varModel = new VariabilityModel(new File("not_existing"), vars);
+        return varModel;
+    }
+
+    /**
+     * Test get original code model file.
+     *
+     * @throws IOException Signals that an I/O exception has occurred.
+     */
+    @Test
+    public void testCollectRelevantElements_irrelevantCb() throws IOException {
+        CodeBlock cb = new CodeBlock(12, 15, new File("file"), not("NOT_IN_VAR_MODEL"), not("NOT_IN_VAR_MODEL"));
+        SourceFileChangeDetector changeDetector = new SourceFileChangeDetector(Consideration.ONLY_VARIABILITY_CHANGE,
+                createTestVariabilityModel(), createTestVariabilityModel());
+        SourceFile<CodeElement<?>> srcFile = new SourceFile<CodeElement<?>>(new File("not_existing"));
+        srcFile.addElement(cb);
+
+        Set<CodeElement<?>> collectedElements = changeDetector.collectRelevantElements(srcFile,
+                new LinuxFormulaRelevancyChecker(changeDetector.varModelA, true));
+
+        Assert.assertThat(collectedElements.size(), CoreMatchers.is(0));
+    }
+
+    /**
+     * Test get original code model file.
+     *
+     * @throws IOException Signals that an I/O exception has occurred.
+     */
+    @Test
+    public void testCollectRelevantElements_relevantCbs() throws IOException {
+        CodeBlock cb = new CodeBlock(12, 15, new File("file"), not("ALPHA"), not("BETA"));
+        CodeBlock cb2 = new CodeBlock(17, 20, new File("file"), not("CONFIG_Test"), not("CONFIG_Tests"));
+        CodeBlock cb3 = new CodeBlock(22, 30, new File("file"), not("Test_MODULE"), not("Test_MODULE"));
+        SourceFileChangeDetector changeDetector = new SourceFileChangeDetector(Consideration.ONLY_VARIABILITY_CHANGE,
+                createTestVariabilityModel(), createTestVariabilityModel());
+        SourceFile<CodeElement<?>> srcFile = new SourceFile<CodeElement<?>>(new File("not_existing"));
+        srcFile.addElement(cb);
+        srcFile.addElement(cb2);
+        srcFile.addElement(cb3);
+
+        Set<CodeElement<?>> collectedElements = changeDetector.collectRelevantElements(srcFile,
+                new LinuxFormulaRelevancyChecker(changeDetector.varModelA, true));
+
+        Assert.assertThat(collectedElements, CoreMatchers.hasItems(cb, cb2, cb3));
+    }
+
+    /**
+     * Test get original code model file.
+     *
+     * @throws IOException Signals that an I/O exception has occurred.
+     */
+    @Test
+    public void testCollectRelevantElements_nestedCbs() throws IOException {
+        // cb and cb_child are relevant because cb is already directly relevant
+        CodeBlock cb = new CodeBlock(12, 15, new File("file"), not("CONFIG_Test"), not("CONFIG_Tests"));
+        CodeBlock cbChild = new CodeBlock(13, 14, new File("file"), not("NOT_IN_VAR_MODEL"), not("NOT_IN_VAR_MODEL"));
+        cb.addNestedElement(cbChild);
+
+        // both blocks are irrelevant
+        CodeBlock cb2 = new CodeBlock(17, 20, new File("file"), not("NOT_IN_VAR_MODEL"), not("NOT_IN_VAR_MODEL"));
+        CodeBlock cb2Child = new CodeBlock(18, 19, new File("file"), not("NOT_IN_VAR_MODEL"), not("NOT_IN_VAR_MODEL"));
+        cb2.addNestedElement(cb2Child);
+
+        // cb3_child is the only element within cb3 that is directly relvevant
+        // however because cb3_child is relevant, cb3 and cb3_child_child are indirectly
+        // relevant.
+        // that leaves us with cb3_child2 being the only irrelevant block within cb3
+        CodeBlock cb3 = new CodeBlock(22, 30, new File("file"), not("NOT_IN_VAR_MODEL"), not("NOT_IN_VAR_MODEL"));
+        CodeBlock cb3Child = new CodeBlock(23, 26, new File("file"), not("CONFIG_Test"), not("CONFIG_Test"));
+        CodeBlock cb3ChildChild =
+                new CodeBlock(24, 25, new File("file"), not("NOT_IN_VAR_MODEL"), not("NOT_IN_VAR_MODEL"));
+        CodeBlock cb3Child2 = new CodeBlock(28, 29, new File("file"), not("NOT_IN_VAR_MODEL"), not("NOT_IN_VAR_MODEL"));
+        cb3.addNestedElement(cb3Child);
+        cb3.addNestedElement(cb3Child2);
+        cb3Child.addNestedElement(cb3ChildChild);
+
+        SourceFile<CodeElement<?>> srcFile = new SourceFile<CodeElement<?>>(new File("not_existing"));
+        srcFile.addElement(cb);
+        srcFile.addElement(cb2);
+        srcFile.addElement(cb3);
+
+        SourceFileChangeDetector changeDetector = new SourceFileChangeDetector(Consideration.ONLY_VARIABILITY_CHANGE,
+                createTestVariabilityModel(), createTestVariabilityModel());
+
+        Set<CodeElement<?>> collectedElements = changeDetector.collectRelevantElements(srcFile,
+                new LinuxFormulaRelevancyChecker(changeDetector.varModelA, true));
+
+        Assert.assertThat(collectedElements, CoreMatchers.hasItems(cb, cbChild, cb3, cb3ChildChild, cb3Child));
+        Assert.assertThat(collectedElements, CoreMatchers.not(CoreMatchers.hasItem(cb2)));
+        Assert.assertThat(collectedElements, CoreMatchers.not(CoreMatchers.hasItem(cb2Child)));
+        Assert.assertThat(collectedElements, CoreMatchers.not(CoreMatchers.hasItem(cb3Child2)));
+        Assert.assertThat(collectedElements.size(), CoreMatchers.is(5));
+    }
+
+    /**
+     * Checks if is structure same any change unchanged.
+     */
+    @Test
+    public void isStructureSame_anyChange_unchanged() {
+        SourceFile<CodeElement<?>> fileA = generateSrcFile(0, false, false);
+        SourceFile<CodeElement<?>> fileB = generateSrcFile(0, false, false);
+
+        SourceFileChangeDetector changeDetector = new SourceFileChangeDetector(Consideration.ANY_CHANGE,
+                createTestVariabilityModel(), createTestVariabilityModel());
+
+        Assert.assertThat(changeDetector.hasChanged(fileA, fileB), CoreMatchers.is(Boolean.FALSE));
+    }
+
+    /**
+     * Checks if is structure same any change changed.
+     */
+    @Test
+    public void isStructureSame_anyChange_changed() {
+        SourceFile<CodeElement<?>> fileA = generateSrcFile(1, false, false);
+        SourceFile<CodeElement<?>> fileB = generateSrcFile(2, false, false);
+
+        SourceFileChangeDetector changeDetector = new SourceFileChangeDetector(Consideration.ANY_CHANGE,
+                createTestVariabilityModel(), createTestVariabilityModel());
+
+        Assert.assertThat(changeDetector.hasChanged(fileA, fileB), CoreMatchers.is(Boolean.TRUE));
+
+    }
+
+    /**
+     * Checks if is structure same any change except line change unchanged.
+     */
+    @Test
+    public void isStructureSame_anyChangeExceptLineChange_unchanged() {
+        SourceFile<CodeElement<?>> fileA = generateSrcFile(1, false, false);
+        SourceFile<CodeElement<?>> fileB = generateSrcFile(0, false, false);
+
+        SourceFileChangeDetector changeDetector = new SourceFileChangeDetector(
+                Consideration.ANY_CHANGE_EXCEPT_LINECHANGE, createTestVariabilityModel(), createTestVariabilityModel());
+
+        Assert.assertThat(changeDetector.hasChanged(fileA, fileB), CoreMatchers.is(Boolean.FALSE));
+    }
+
+    /**
+     * Checks if is structure same any change except line change changed.
+     */
+    @Test
+    public void isStructureSame_anyChangeExceptLineChange_changed() {
+        SourceFile<CodeElement<?>> fileA = generateSrcFile(1, true, false);
+        SourceFile<CodeElement<?>> fileB = generateSrcFile(0, false, false);
+
+        SourceFileChangeDetector changeDetector = new SourceFileChangeDetector(
+                Consideration.ANY_CHANGE_EXCEPT_LINECHANGE, createTestVariabilityModel(), createTestVariabilityModel());
+
+        Assert.assertThat(changeDetector.hasChanged(fileA, fileB), CoreMatchers.is(Boolean.TRUE));
+    }
+
+    /**
+     * Checks if is structure same only variability change unchanged.
+     */
+    @Test
+    public void isStructureSame_onlyVariabilityChange_unchanged() {
+        SourceFile<CodeElement<?>> fileA = generateSrcFile(1, true, false);
+        SourceFile<CodeElement<?>> fileB = generateSrcFile(2, false, false);
+
+        SourceFileChangeDetector changeDetector = new SourceFileChangeDetector(Consideration.ONLY_VARIABILITY_CHANGE,
+                createTestVariabilityModel(), createTestVariabilityModel());
+
+        Assert.assertThat(changeDetector.hasChanged(fileA, fileB), CoreMatchers.is(Boolean.FALSE));
+
+    }
+
+    /**
+     * Checks if is structure same only variability change changed.
+     */
+    @Test
+    public void isStructureSame_onlyVariabilityChange_changed() {
+        SourceFile<CodeElement<?>> fileA = generateSrcFile(1, true, true);
+        SourceFile<CodeElement<?>> fileB = generateSrcFile(2, false, false);
+
+        SourceFileChangeDetector changeDetector = new SourceFileChangeDetector(Consideration.ONLY_VARIABILITY_CHANGE,
+                createTestVariabilityModel(), createTestVariabilityModel());
+
+        Assert.assertThat(changeDetector.hasChanged(fileA, fileB), CoreMatchers.is(Boolean.TRUE));
+
+    }
+
+    /**
+     * Generate src file.
+     *
+     * @param lineNumberModificator the line number modificator
+     * @param modifyIrrelevantBlock the modify irrelevant block
+     * @param modifyRelevantBlock   the modify relevant block
+     * @return the source file
+     */
+    public SourceFile<CodeElement<?>> generateSrcFile(int lineNumberModificator, boolean modifyIrrelevantBlock,
+            boolean modifyRelevantBlock) {
+        // cb and cb_child are relevant because cb is already directly relevant
+        CodeBlock cb = new CodeBlock(12 + lineNumberModificator, 15 + lineNumberModificator, new File("file"),
+                not("CONFIG_Test"), not("CONFIG_Tests"));
+        CodeBlock cbChild = new CodeBlock(13 + lineNumberModificator, 14 + lineNumberModificator, new File("file"),
+                not("NOT_IN_VAR_MODEL"), not("NOT_IN_VAR_MODEL"));
+        cb.addNestedElement(cbChild);
+
+        // cb3_child is the only element within cb3 that is directly relvevant
+        // however because cb3_child is relevant, cb3 and cb3_child_child are indirectly
+        // relevant.
+        // that leaves us with cb3_child2 being the only irrelevant block within cb3
+        CodeBlock cb3 = new CodeBlock(22 + lineNumberModificator, 30 + lineNumberModificator, new File("file"),
+                not("NOT_IN_VAR_MODEL"), not("NOT_IN_VAR_MODEL"));
+        CodeBlock cb3Child = new CodeBlock(23 + lineNumberModificator, 26 + lineNumberModificator, new File("file"),
+                not("CONFIG_Test"), not("CONFIG_Test"));
+        CodeBlock cb3ChildChild = new CodeBlock(24 + lineNumberModificator, 25 + lineNumberModificator,
+                new File("file"), not("NOT_IN_VAR_MODEL"), not("NOT_IN_VAR_MODEL"));
+        CodeBlock cb3Child2 = new CodeBlock(28 + lineNumberModificator, 29 + lineNumberModificator, new File("file"),
+                not("NOT_IN_VAR_MODEL"), not("NOT_IN_VAR_MODEL"));
+        cb3.addNestedElement(cb3Child);
+        if (!modifyIrrelevantBlock) {
+            cb3.addNestedElement(cb3Child2);
+        }
+        if (!modifyRelevantBlock) {
+            cb3Child.addNestedElement(cb3ChildChild);
+        }
+
+        SourceFile<CodeElement<?>> srcFile = new SourceFile<CodeElement<?>>(new File("not_existing"));
+        srcFile.addElement(cb);
+
+        if (!modifyIrrelevantBlock) {
+            // both blocks are irrelevant
+            CodeBlock cb2 = new CodeBlock(17 + lineNumberModificator, 20 + lineNumberModificator, new File("file"),
+                    not("NOT_IN_VAR_MODEL"), not("NOT_IN_VAR_MODEL"));
+            CodeBlock cb2Child = new CodeBlock(18 + lineNumberModificator, 19 + lineNumberModificator, new File("file"),
+                    not("NOT_IN_VAR_MODEL"), not("NOT_IN_VAR_MODEL"));
+            cb2.addNestedElement(cb2Child);
+            srcFile.addElement(cb2);
+        }
+        srcFile.addElement(cb3);
+        return srcFile;
+    }
+
+}
